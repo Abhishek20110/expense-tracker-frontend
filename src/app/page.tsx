df8e2a84176bf6efee1a2b81eeab183e450e6d3e
Abhishek Dey <71166991+Abhishek20110@users.noreply.github.com>
2025-02-12 15:51:01 +0530

    remove error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -331,13 +331,6 @@ const HomePage: React.FC = () => {
                     )}
                 </div>
             </div>
-
-            {/* Error Display */}
-            {error && (
-                <div className="fixed top-4 right-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded">
-                    {error}
-                </div>
-            )}
         </div>
     );
 };
